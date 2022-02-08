197ea5e499a243b6f57f2934fe3e85381e97cb53
arne <armaxri@gmail.com>
2022-02-08 22:07:47 +0100

    Improved the example.

--- test_workspaces/workspace05/stdioCheck.py
+++ test_workspaces/workspace05/stdioCheck.py
@@ -4,9 +4,13 @@ import sys
 import time
 
 print("Hello from stdout!")
+sys.stdout.flush()
 print("Hello from stderr!", file=sys.stderr)
+sys.stderr.flush()
 
 time.sleep(3)
 
 print("Bye from stdout!")
+sys.stdout.flush()
 print("Bye from stderr!", file=sys.stderr)
+sys.stderr.flush()
