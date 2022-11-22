61b9edab7457d116b03e079e0fd470edf51a4eea
arne <armaxri@gmail.com>
2022-11-23 00:31:45 +0100

    Improved testing environment.

--- test_workspaces/workspace06/testCnt.py
+++ test_workspaces/workspace06/testCnt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 import sys
 import time
 import argparse
@@ -11,6 +12,13 @@ def printAndFlush(msg):
 
 def run_counting(args):
     printAndFlush(f"Hello from test stript instance '{args.name}'!")
+
+    foo_value = os.getenv('FOO')
+    if foo_value:
+        printAndFlush(f"Environment variable with the name 'FOO' was set to the value '{foo_value}'.")
+    else:
+        printAndFlush("No environment variable with the name 'FOO' was set.")
+
     for i in range(args.count):
         printAndFlush(f"Instance '{args.name}' counted to '{i}'.")
         time.sleep(1)
